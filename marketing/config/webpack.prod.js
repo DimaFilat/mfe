92f3c670b9cf90a80411337b3a1f0db3ab1b3276
Dmitriy Filatchev <o6jlako@mail.ru>
2022-01-02 08:03:57 +0300

    create container deploy script

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const sharedDependencies = Object.keys(packageJson.dependencies);
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: sharedDependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
