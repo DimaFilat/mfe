f7df612c50ca8a8ceee7dff3809e72d9cf3f7e96
Dmitriy Filatchev <o6jlako@mail.ru>
2022-01-02 10:29:33 +0300

    feat: add header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <div>
-      <h1>MFE application</h1>
-      <MarketingApp />
-    </div>
+    <Router>
+      <>
+        <Header />
+        <MarketingApp />
+      </>
+    </Router>
   );
 };
