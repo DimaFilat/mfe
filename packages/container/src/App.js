e1b63c2455a6a11711d68df11844fdf5e650b386
Dmitriy Filatchev <o6jlako@mail.ru>
2022-01-02 11:00:12 +0300

    fix: production collision prod classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,23 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
   return (
-    <Router>
-      <>
-        <Header />
-        <MarketingApp />
-      </>
-    </Router>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+        <>
+          <Header />
+          <MarketingApp />
+        </>
+      </Router>
+    </StylesProvider>
   );
 };
